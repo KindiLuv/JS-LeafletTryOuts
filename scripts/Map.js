2747a7c135133558d7a91ab3728e582766353520
PaydiLuv <anykennylster@gmail.com>
2021-02-09 13:45:08 +0100

    Localisation tool added
    Refector started, localisation tool is implemented to find all the future marker positions

---
+++ scripts/Map.js
@@ -0,0 +1,30 @@
+// Variables
+var mapSW = L.LatLng(-79.1, -180),
+mapNE = L.LatLng(79.1, 180),
+bounds = L.LatLngBounds(mapSW,mapNE);
+
+// Declare Map Object
+var map = L.map('map').setView([0, 0],1);
+
+// Reference the Tiles
+L.tileLayer('./media/MapTiles/{z}/{x}/{y}.png', {
+minZoom: 1,
+maxZoom: 4,
+continuousWorld: true,
+noWrap: true,
+crs: L.CRS.Simple,
+bounds:[[-79.1,-176],[79.1,176]],
+maxBoundViscosity: 1.0
+}).addTo(map);
+
+// Proper map display
+map.fitBounds([
+[[-128,-128],[128,128]]
+]);
+map.setMaxBounds([[-79.1,-176],[79.1,176]]);
+
+// Add markers, pop-ups and else.
+var marker = L.marker([0,0],{
+draggable: true
+}).addTo(map);
+marker.bindPopup('<b>You are here</b>');
