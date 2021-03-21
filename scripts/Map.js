c67f6b30f49367ba4feea80b88829653f849bcef
William <zwilliamcl@gmail.com>
2021-03-22 00:17:01 +0100

    Selector

--- scripts/Map.js
+++ scripts/Map.js
@@ -1,3 +1,6 @@
+var allPoly = [];
+
+
 function createLocationMarker(map) {
   // Add markers, pop-ups and else.
   var marker = L.marker([0, 0], {
@@ -12,8 +15,7 @@ function createLocationMarker(map) {
   });
 }
 
-function parsemarker(map, markersCheckbox){
-  if (markersCheckbox.checked == true) {
+function parsemark(map){
     $.ajax({
       dataType: 'json',
       url: '/data/markers.json',
@@ -25,30 +27,31 @@ function parsemarker(map, markersCheckbox){
       }
     });
   }
-  else {
-    $.ajax({
-      dataType: 'json',
-      url: '/data/markers.json',
-      success: function(data){
-        for(var i = 0; i < 70; ++i){
-          var point = L.marker(data.features[i].geometry.coordinates,{/*{option*/ opacity: 0}).addTo(map);
-          point.bindPopup("<b>"+data.features[i].properties.name);
-        }
-      }
-    });
-  }
-}
 
-function parsepolygon(map, markersCheckbox){
+function parsepolygon(map){
   $.ajax({
     dataType: 'json',
     url: '/data/polygons.json',
     success: function(data){
       console.log(data.features[0].geometry.coordinates);
-      for(var i = 0; i < 70; ++i){
-        var polygon = L.polygon(data.features[i].geometry.coordinates,{/*option*/ opacity: 1}).addTo(map);
+      for(var i = 0; i < 17; ++i){
+        var polygon = L.polygon(data.features[i].geometry.coordinates,{/*option*/ opacity: 1});
         polygon.bindPopup("<b>"+data.features[i].properties.name);
+        allPoly.push(polygon);
+        $('#tickmap').append("<input type='checkbox' name='markersCheckbox' id='" + i + "' class='polygonesLayers'> <label for='markers'>" + i/*`${data.features[i].properties.name}`*/ + "</label>")
       }
+      $('.polygonesLayers').change(function f(){
+        var id = this.id;
+        if(this.checked){
+          allPoly[id].addTo(map);
+        }
+        else{
+          allPoly[id].remove();
+        }
+      })
     }
   });
 }
+
+
+
